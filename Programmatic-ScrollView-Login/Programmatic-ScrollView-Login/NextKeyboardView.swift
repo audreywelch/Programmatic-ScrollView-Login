2ae1ef63be9cd4343e50c115afa699ce33d8e141
audreywelch <welch.audreyl@gmail.com>
2020-06-13 20:55:24 -0700

    Attempted adding a sticky navigation header

---
+++ Programmatic-ScrollView-Login/Programmatic-ScrollView-Login/NextKeyboardView.swift
@@ -0,0 +1,13 @@
+//
+//  NextKeyboardView.swift
+//  Programmatic-ScrollView-Login
+//
+//  Created by Audrey Welch on 6/13/20.
+//  Copyright © 2020 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+protocol NextView: UIView {
+    var nextView: UIView? { get set }
+}
--- Programmatic-ScrollView-Login/Programmatic-ScrollView-Login/ViewController.m
+++ Programmatic-ScrollView-Login/Programmatic-ScrollView-Login/ViewController.m
@@ -9,10 +9,12 @@
 #import "ViewController.h"
 #import "Programmatic_ScrollView_Login-Swift.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate, UIScrollViewDelegate>
 
 - (void)updateViews;
 - (void)addImageView;
+
+@property (nonatomic)UIImageView *topImageView;
 
 @end
 
@@ -21,6 +23,9 @@
 CGFloat imageHeight = 300;
 CGFloat buttonheight = 60;
 CGFloat textFieldHeight = 40;
+CGFloat stackViewPadding = 20;
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -37,7 +42,10 @@ CGFloat textFieldHeight = 40;
     scrollView.translatesAutoresizingMaskIntoConstraints = NO;
     scrollView.alwaysBounceVertical = YES;
     scrollView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+    scrollView.delaysContentTouches = NO;
     scrollView.contentInset = UIEdgeInsetsMake(imageHeight, 0, 0, 0);
+    scrollView.delegate = self;
+    scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
     
     [self.view addSubview:scrollView];
     
@@ -63,8 +71,8 @@ CGFloat textFieldHeight = 40;
     stackView.translatesAutoresizingMaskIntoConstraints = NO;
     stackView.axis = UILayoutConstraintAxisVertical;
     stackView.alignment = UIStackViewAlignmentFill;
-    stackView.spacing = 8;
-    stackView.directionalLayoutMargins = NSDirectionalEdgeInsetsMake(20, 20, 20, 20);
+    stackView.spacing = 16;
+    stackView.directionalLayoutMargins = NSDirectionalEdgeInsetsMake(stackViewPadding, stackViewPadding, stackViewPadding, stackViewPadding);
     stackView.layoutMarginsRelativeArrangement = YES;
 
     [contentView addSubview:stackView];
@@ -79,16 +87,21 @@ CGFloat textFieldHeight = 40;
     UITextField *nameTextField = [[PaddedTextField alloc] init];
     nameTextField.translatesAutoresizingMaskIntoConstraints = NO;
     nameTextField.placeholder = @"Name";
+    nameTextField.delegate = self;
     [self.view addSubview:nameTextField];
     
     UITextField *passwordTextField = [[PaddedTextField alloc] init];
     passwordTextField.translatesAutoresizingMaskIntoConstraints = NO;
     passwordTextField.placeholder = @"Password";
+    passwordTextField.delegate = self;
     passwordTextField.secureTextEntry = YES;
     
     // Text Field Constraints
     [nameTextField.heightAnchor constraintEqualToConstant:textFieldHeight].active = YES;
     [passwordTextField.heightAnchor constraintEqualToConstant:textFieldHeight].active = YES;
+    
+    nameTextField.nextView = passwordTextField;
+    passwordTextField.nextView = nil;
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
     [button setBackgroundImage:[UIImage imageNamed:@"blue-button"] forState:UIControlStateNormal];
@@ -96,6 +109,7 @@ CGFloat textFieldHeight = 40;
     [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     button.titleLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightBold];
     button.adjustsImageWhenHighlighted = YES;
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     
     [stackView addArrangedSubview:nameTextField];
     [stackView addArrangedSubview:passwordTextField];
@@ -103,15 +117,45 @@ CGFloat textFieldHeight = 40;
     
 }
 
+- (void)buttonPressed:(UIButton *)button {
+    
+    NSLog(@"Log In!");
+    [self.view endEditing:YES];
+}
+
 - (void)addImageView {
     
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"kalen-emsley-mountain"]];
     
     imageView.frame = CGRectMake(0, 0, self.view.bounds.size.width, imageHeight);
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.clipsToBounds = YES;
+    
+    _topImageView = imageView;
     
     [self.view addSubview:imageView];
     
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGFloat height = -scrollView.contentOffset.y;
+    
+    if (height > imageHeight) {
+        // stretch it longer using contentFill
+        _topImageView.frame.size.height = height;
+    } else {
+        // push it offscreen by changing the y coordinate
+        self.topImageView.frame.origin.y = height - imageHeight;
+    }
+}
+
 
 @end
+
+
